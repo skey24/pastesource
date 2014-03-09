7263a0e14dda516241706cd7009376ec5bcc4c20
Sergey Kulichkin <s.kulichkin@gmail.com>
2014-03-09 18:48:12 +0500

    Added post query and store in MongoDB by mongoid

--- app.rb
+++ app.rb
@@ -1,21 +1,46 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'mongoid'
+
+configure do
+  Mongoid.load!("./mongoid.yml")
+end
+
+class Paste
+  include Mongoid::Document
+  field :id, type: String
+  field :text,   type: String
+  field :private, type: Boolean
+end
+
 
 get '/' do
   # form for enter code
   erb :post, :layout => :layout
 end
 
-get '/:paste' do
+get '/:id' do
   #show paste
+  @paste = Paste.find(params[:id])
+  @text = @paste.text
+  
 end
 
 post '/paste' do
   #generating address 
-  paste_id = (('a'..'z').to_a+('A'..'Z').to_a+(0..9).to_a).shuffle[0,8].join
-  
+  @paste_id = (('a'..'z').to_a+('A'..'Z').to_a+(0..9).to_a).shuffle[0,8].join
   @text = params[:text]
+  @private = params[:private]
+  paste = Paste.new
+  paste.text = @text
+  paste.id = @paste_id
+  paste.private = @private
+  paste.save
   
+  redirect "/#{@paste_id}"
+end
+
+get '/all' do
   
 end
